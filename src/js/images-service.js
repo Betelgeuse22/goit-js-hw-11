f9e15da2415f1ef960e3816966b5e84c591bd884
Betelgeuse22 <Don.vitaki@gmail.com>
2022-10-25 22:22:12 +0200

    HW-11

---
+++ src/js/images-service.js
@@ -0,0 +1,59 @@
+import axios from 'axios';
+
+const API_KEY = '30791377-a48f7d7a4f3867fd227b1c1aa';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export default class ImagesApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.loadedHits = 0;
+  }
+
+  async fetchImages() {
+    const searchParams = new URLSearchParams({
+      key: API_KEY,
+      q: this.searchQuery,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page: this.page,
+      per_page: 40,
+    });
+
+    const url = `${BASE_URL}?${searchParams}`;
+
+    try {
+      const response = await axios.get(url);
+      this.incrementPage();
+
+      return response.data;
+    } catch (error) {
+      console.warn(`${error}`);
+    }
+  }
+
+  incrementLoadedHits(hits) {
+    this.loadedHits += hits.length;
+  }
+
+  resetLoadedHits() {
+    this.loadedHits = 0;
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
